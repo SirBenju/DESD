ecaf1a54bc31e890df3c34aded75d2edb01236c4
b34-evans <benjamin6.evans@live.uwe.ac.uk>
2024-03-05 14:02:32 +0000

    Add Django project to repository

---
+++ smartcare_surgery/login/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class CustomAuthenticationForm(AuthenticationForm):
+    # add any customizations you need
+    pass
